adaadc805231ac9ab619385aa57f9471ef1424b6
anonymous-name <anonymous-email@example.com>
2025-03-14 22:45:57 +0100

    feat: embed event calendar and store map

--- src/components/StoreMap.vue
+++ src/components/StoreMap.vue
@@ -1,5 +1,11 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="h-full bg-pink-500 flex-grow"><iframe src="https://www.google.com/maps/d/u/4/embed?mid=1xhpqfJQDw9wmWJh7aSEk--Z4hGM7ly8&ehbc=2E312F&noprof=1" height="480" class="w-screen lg:w-xl"></iframe></div>
+  <div class="h-full bg-pink-500 flex-grow">
+    <iframe
+      src="https://www.google.com/maps/d/u/4/embed?mid=1xhpqfJQDw9wmWJh7aSEk--Z4hGM7ly8&ehbc=2E312F&noprof=1"
+      height="480"
+      class="w-screen lg:w-xl"
+    ></iframe>
+  </div>
 </template>
