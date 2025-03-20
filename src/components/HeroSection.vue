c13eec062f9cfb9db7a82e2f5c7525db8262f9e1
anonymous-name <anonymous-email@example.com>
2025-03-20 15:54:06 +0100

    feat: add progress bar

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,14 +1,21 @@
 <script setup lang="ts">
 import CountdownClock from '@/components/CountdownClock.vue'
+import ProgressBar from '@/components/ProgressBar.vue';
 </script>
 
 <template>
-  <div class="bg-blue-500 p-5 flex flex-col lg:flex-row">
+  <div class="w-full bg-blue-500 p-5 flex flex-col justify-around items-center lg:flex-row">
     <div class="space-y-5 flex flex-col justify-center">
       <h1 class="font-display text-xl lg:text-3xl">
         罷免二階即將截止！即刻行動！
       </h1>
       <CountdownClock />
+    </div>
+    <div class="space-y-5 flex flex-col justify-centers">
+      <h1 class="font-display text-xl lg:text-3xl">
+        尚須 60,000 份
+      </h1>
+      <ProgressBar />
     </div>
   </div>
 </template>
---
+++ src/components/ProgressBar.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const recallPetitionAmounts = ref(10000)
+const progressStyle = recallPetitionAmounts.value < 35000 ? "progress-error" : "progress-warning"
+</script>
+
+<template>
+  <progress class="progress progress-error w-70 h-15 lg:w-96" :class="{progress: progressStyle}" :value="recallPetitionAmounts" max="70000"></progress>
+</template>
