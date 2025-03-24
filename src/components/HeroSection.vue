63edb0c61d55b1d625af9ab606e8326fb5bafffd
anonymous-name <anonymous-email@example.com>
2025-03-24 15:52:48 +0100

    feat: change hero image and change some css style

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -4,17 +4,16 @@ import ProgressBar from '@/components/ProgressBar.vue';
 </script>
 
 <template>
-  <div class="w-full bg-blue-500 p-5 flex flex-col justify-around items-center lg:flex-row">
+  <div
+  class="hero w-full p-5 flex flex-col justify-around items-center bg-contain lg:flex-row"
+  style="background-image: url(https://scontent-ams2-1.xx.fbcdn.net/v/t39.30808-6/484339805_122141273546387506_2620765745331271053_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=127cfc&_nc_ohc=-8VL4AFOYLYQ7kNvgEUyr6t&_nc_oc=Adk26R-19W1XnWDC66APLESr0EglrHupW4c5lMDTg-QNPmJ00vVRBJ4bTqHlrw-QZLs&_nc_zt=23&_nc_ht=scontent-ams2-1.xx&_nc_gid=BhUqUv4YFbQISQj-83fA-w&oh=00_AYHsqX0wTX9jpDU75_U4dt-fT0fJ3gK9HNhBY0OWxEhcPw&oe=67E73F52);">
     <div class="space-y-5 flex flex-col justify-center">
-      <h1 class="font-display text-xl lg:text-3xl">
+      <h1 class="w-fit font-display font-extrabold text-xl bg-amber-100 lg:text-3xl">
         罷免二階即將截止！即刻行動！
       </h1>
       <CountdownClock />
     </div>
     <div class="space-y-5 flex flex-col justify-centers">
-      <h1 class="font-display text-xl lg:text-3xl">
-        尚須 60,000 份
-      </h1>
       <ProgressBar />
     </div>
   </div>
--- src/components/ProgressBar.vue
+++ src/components/ProgressBar.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 
-const recallPetitionAmounts = ref(10000)
+const recallPetitionAmounts = ref(10786)
 const progressStyle = recallPetitionAmounts.value < 35000 ? "progress-error" : "progress-warning"
 </script>
 
 <template>
+  <h1 class="w-fit font-display text-xl bg-amber-100 lg:text-3xl">
+    尚須 {{ 70000 - recallPetitionAmounts }} 份
+  </h1>
   <progress class="progress progress-error w-70 h-15 lg:w-96" :class="{progress: progressStyle}" :value="recallPetitionAmounts" max="70000"></progress>
 </template>
