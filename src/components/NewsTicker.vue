43c57f4f45c6786da20fb22c92e43b2ff0ddfa25
anonymous-name <anonymous-email@example.com>
2025-03-26 15:48:52 +0100

    feat: add marquee and run the format

--- src/components/NewsTicker.vue
+++ src/components/NewsTicker.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="bg-warning flex items-center space-x-2 p-2">
+  <!-- <div class="bg-warning flex items-center space-x-2 p-2">
     <svg
       xmlns="http://www.w3.org/2000/svg"
       class="h-6 w-6 shrink-0 stroke-current"
@@ -16,5 +16,33 @@
       />
     </svg>
     <span class="font-display">快訊：敲羅行動受壓迫！市場攤位租金已付清仍遭刁難裁撤！！！</span>
+  </div> -->
+
+  <div class="relative overflow-hidden bg-warning flex items-center space-x-2 h-8">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      class="absolute left-0 h-6 w-6 shrink-0 stroke-current bg-warning z-20"
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+      />
+    </svg>
+    <div class="animate-marquee whitespace-nowrap font-display">
+      <span class="mx-4">快訊：敲羅行動受壓迫，市場攤位租金已付清仍遭刁難裁撤！</span>
+      <span class="mx-4">快訊：羅廷瑋大學老師投書自由時報具名呼籲小羅莫忘從政初心，勇於認錯！</span>
+      <span class="mx-4">第二階段已開跑，而且快結束惹！！！</span>
+      <span class="mx-4">齊心敲羅慧成宮！！！</span>
+    </div>
+    <div class="absolute animate-marquee2 whitespace-nowrap font-display">
+      <span class="mx-4">快訊：敲羅行動受壓迫，市場攤位租金已付清仍遭刁難裁撤！</span>
+      <span class="mx-4">快訊：羅廷瑋大學老師投書自由時報具名呼籲小羅莫忘從政初心，勇於認錯！</span>
+      <span class="mx-4">第二階段已開跑，而且快結束惹！！！ </span>
+      <span class="mx-4">齊心敲羅慧成宮！！！</span>
+    </div>
   </div>
 </template>
